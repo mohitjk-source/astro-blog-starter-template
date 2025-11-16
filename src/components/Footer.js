fb61408656ef49bb2f6fa0372c1395f51d509d16
Mohit.jk <mohit.jk@gmail.com>
2025-11-16 21:04:50 +0530

    Update and rename Footer.astro to Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="social-icons">
+        <a href="https://www.youtube.com/@soulwithoutjhol" target="_blank"><img src="/assets/youtube-icon.svg" alt="YouTube" /></a>
+        <a href="https://www.facebook.com/soulwithoutjhol" target="_blank"><img src="/assets/facebook-icon.svg" alt="Facebook" /></a>
+        <a href="https://www.instagram.com/soulwithoutjhol" target="_blank"><img src="/assets/instagram-icon.svg" alt="Instagram" /></a>
+        <a href="https://twitter.com/soulwithoutjhol" target="_blank"><img src="/assets/x-icon.svg" alt="X" /></a>
+      </div>
+      <a href="https://www.instagram.com/soulwithoutjhol" className="follow-btn" target="_blank">Follow us</a>
+      <p>© 2025 SWJ. All rights reserved.</p>
+    </footer>
+  );
+}
